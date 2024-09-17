bb0d12494054cf4b6d0ec7d0b393f4ff317ace99
Ming Jun <evan.zhangmingjun@gmail.com>
2024-09-18 03:13:27 +0800

    build: set up repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vite-React-Typescript-Starter
+
+This template provides a minimal setup to get React working in Vite with Tailwind, HMR and some ESLint rules.
+
+This was made largely for my personal use, to streamline the process of setting up a barebone React + Typescript + Tailwind app with Vite, usually for quick testing/prototyping. This may get more fleshed out in the future, though I don't have a timeline for it.
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen flex-col place-content-center place-items-center gap-4">
+      <h1 className="text-3xl font-bold">
+        Tailwind is working as intended{" "}
+        <span className="underline">if this is underlined</span>
+      </h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HM
+      </p>
+    </div>
+  );
+}
+
+export default App;
